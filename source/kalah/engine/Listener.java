0f75835b0459fd2fbce85da830bc71f329c8cb09
Mathew Campbell <curiousinternals@gmail.com>
2015-11-23 13:45:47 +0000

    Modified in/out to use stdin and stdout. Resolves #19

--- source/kalah/engine/Listener.java
+++ source/kalah/engine/Listener.java
@@ -4,32 +4,29 @@ import kalah.Controller;
 import kalah.engine.message.engine.EngineMessageFactory;
 
 import java.io.*;
-import java.net.Socket;
 
 /**
- * Listens to a given socket and passes messages back to its owner.
+ * Listens to stdin and passes messages back to its owner.
  */
-public class Listener {
-
+public class Listener
+{
   private final Controller controller;
-  private final Socket socket;
+  private final BufferedReader input;
 
-  public Listener(Socket socket, Controller controller) {
+  public Listener(Controller controller)
+  {
     this.controller = controller;
-    this.socket = socket;
+    this.input = new BufferedReader(new InputStreamReader(System.in));
   }
 
   /**
-   * Makes the listener listen to its socket forever and send messages back to
-   * its owner.
+   * Makes the listener listen forever and send messages to its owner.
    */
-  public void listen() throws IOException {
-    BufferedReader input =
-      new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-    while (true) {
+  public void listen() throws IOException
+  {
+    while (true)
+    {
       controller.handle(EngineMessageFactory.getMessage(input.readLine()));
     }
   }
-
 }
