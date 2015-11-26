543cdf283448cdf56fdb6a323fe83ba4011d38c9
DarkholmeTenk <shaney.booth@gmail.com>
2015-11-26 04:33:41 +0000

    Logical fix for monte carlo test
    Should check total score at the end, not after each game

--- test/kalah/test/agents/SimpleMonteCarloTest.java
+++ test/kalah/test/agents/SimpleMonteCarloTest.java
@@ -3,6 +3,10 @@ package kalah.test.agents;
 import static org.junit.Assert.*;
 import kalah.agent.RandomAgent;
 import kalah.agent.SimpleMonteCarloAgent;
+import kalah.game.board.Action;
+import kalah.game.board.BoardState;
+import kalah.game.board.Player;
+import kalah.program.Configuration;
 import kalah.program.TwoAgentGame;
 
 import org.junit.Test;
@@ -14,14 +18,13 @@ public class SimpleMonteCarloTest extends AgentTest
 	@Test
 	public void test()
 	{
+		Configuration.maxTimePerTurn = 4;
 		TwoAgentGame game = new TwoAgentGame(SimpleMonteCarloAgent.playerOne, RandomAgent.playerTwo);
 		double score = 0;
 		for(int i = 0; i < runs; i++)
-		{
-			if(score < 0)
-				fail("No way the monte carlo should have lost");
 			score += game.play(start) / (double) runs;
-		}
+		if(score < 0)
+			fail("No way the monte carlo should have lost");
 		System.out.println("Overall score: " + score);
 	}
 
