960da8e94dede1cc8205f9649a1ee55982518126
Shane Booth <shane.booth@student.manchester.ac.uk>
2015-11-27 15:12:20 +0000

    Tests filled in

--- test/kalah/test/agents/factories/FactoryFactoryTest.java
+++ test/kalah/test/agents/factories/FactoryFactoryTest.java
@@ -1,6 +1,10 @@
 package kalah.test.agents.factories;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+import kalah.agent.factories.AbstractAgentFactory;
+import kalah.agent.factories.AbstractAgentFactoryFactory;
+import kalah.exceptions.NoSuchAgentException;
 
 import org.junit.Test;
 
@@ -8,9 +12,24 @@ public class FactoryFactoryTest
 {
 
 	@Test
-	public void test()
+	public void getAgentFactoryTest()
 	{
-		fail("Not yet implemented");
+		AbstractAgentFactory factory = AbstractAgentFactoryFactory.get("puremc");
+		assertNotNull("No factory given", factory);
+	}
+
+	@Test
+	public void nonExistentAgentTest()
+	{
+		try
+		{
+			AbstractAgentFactory factory = AbstractAgentFactoryFactory.get("neverregisterthispls");
+			fail("We actually got a thing?");
+		}
+		catch(NoSuchAgentException e)
+		{
+
+		}
 	}
 
 }
