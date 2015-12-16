f94375b1cecf31d194f098fc39e0089ddf6ee4ea
DarkholmeTenk <shaney.booth@gmail.com>
2015-12-16 20:22:51 +0000

    Record moves we're making

--- source/kalah/engine/Speaker.java
+++ source/kalah/engine/Speaker.java
@@ -1,6 +1,7 @@
 package kalah.engine;
 
 import kalah.engine.message.agent.AgentMessage;
+import kalah.program.Configuration;
 
 import java.io.*;
 
@@ -9,20 +10,21 @@ import java.io.*;
  */
 public class Speaker
 {
-  private final DataOutputStream output;
+	private final DataOutputStream output;
 
-  public Speaker(PrintStream stream)
-  {
-    this.output = new DataOutputStream(stream);
-  }
+	public Speaker(PrintStream stream)
+	{
+		output = new DataOutputStream(stream);
+	}
 
-  /**
-   * Makes speaker say something on its output
-   *
-   * @param message The AgentMessage to send to the game engine
-   */
-  public void say(AgentMessage message) throws IOException
-  {
-    output.writeBytes(message.toString());
-  }
+	/**
+	 * Makes speaker say something on its output
+	 *
+	 * @param message The AgentMessage to send to the game engine
+	 */
+	public void say(AgentMessage message) throws IOException
+	{
+		Configuration.log("Making move: " + message);
+		output.writeBytes(message.toString());
+	}
 }
