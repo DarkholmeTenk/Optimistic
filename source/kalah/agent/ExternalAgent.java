ee272e34e8fa0b310ca8cadb892dd1136bc1ac1f
DarkholmeTenk <shaney.booth@gmail.com>
2015-12-16 20:38:21 +0000

    Redid output

--- source/kalah/agent/ExternalAgent.java
+++ source/kalah/agent/ExternalAgent.java
@@ -5,55 +5,58 @@ import kalah.engine.Speaker;
 import kalah.engine.message.engine.*;
 import kalah.engine.message.agent.*;
 import kalah.game.board.*;
+import kalah.program.Configuration;
 
 import java.io.IOException;
 
 public class ExternalAgent extends AbstractAgent
 {
-  private final Listener listener;
-  private final Speaker speaker;
+	private final Listener listener;
+	private final Speaker speaker;
 
-  public ExternalAgent(Player player, Listener listener, Speaker speaker)
-  {
-    super(player);
-    this.listener = listener;
-    this.speaker = speaker;
-  }
+	public ExternalAgent(Player player, Listener listener, Speaker speaker)
+	{
+		super(player);
+		this.listener = listener;
+		this.speaker = speaker;
+	}
 
-  @Override
-  public void opponentAction(BoardState state, Action action)
-  {
-    try
-    {
-      if(action instanceof SwapAction)
-        speaker.say(new SwapCommandMessage());
-      else
-        speaker.say(new MoveCommandMessage(action));
-    }
-    catch (IOException e)
-    {
-        throw new RuntimeException("Speaker broke! " + e);
-    }
-  }
+	@Override
+	public void opponentAction(BoardState state, Action action)
+	{
+		try
+		{
+			Configuration.log(state.toString());
+			Configuration.log("Action: " + action.toString());
+			if(action instanceof SwapAction)
+				speaker.say(new SwapCommandMessage());
+			else
+				speaker.say(new MoveCommandMessage(action));
+		}
+		catch (IOException e)
+		{
+			throw new RuntimeException("Speaker broke! " + e);
+		}
+	}
 
-  @Override
-  public Action getNextMove(BoardState board)
-  {
-    try
-    {
-      EngineMessage message = listener.next();
+	@Override
+	public Action getNextMove(BoardState board)
+	{
+		try
+		{
+			EngineMessage message = listener.next();
 
-      if(message instanceof MoveMessage)
-        return new Action(this.agentPlayer, ((MoveMessage) message).getHouse());
-      else if (message instanceof SwapMessage)
-        return new SwapAction();
-      else
-        return null;
-    }
-    catch (IOException e)
-    {
-      throw new RuntimeException("Listener broke! " + e);
-    }
-  }
+			if(message instanceof MoveMessage)
+				return new Action(agentPlayer, ((MoveMessage) message).getHouse());
+			else if (message instanceof SwapMessage)
+				return new SwapAction();
+			else
+				return null;
+		}
+		catch (IOException e)
+		{
+			throw new RuntimeException("Listener broke! " + e);
+		}
+	}
 
 }
